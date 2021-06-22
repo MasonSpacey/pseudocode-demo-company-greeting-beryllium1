961bd60a921cac64b38b0a000d8a8f747380354a
Mason <masonsj96@gmail.com>
2021-06-22 11:35:42 -0400

    Added slight JS, committing in order to git pull

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+let dayWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+
+
+
+if (dayWeek[0]) {
+    document.write(`Looks like somebody's got a case of the Monday's...`)
+} else if (dayWeek[1]) {
+    document.write(`Another day, another <strike>Domination</strike> Dollar!`)
+} else if (dayWeek[2]) {
+    document.write(`Hump Dayyyyy`)
+} 
+    
